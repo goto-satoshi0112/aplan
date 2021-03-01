6b5c8abf0ffa22a49665922e23e548b9c9b793f4
goto-satoshi0112 <73925600+goto-satoshi0112@users.noreply.github.com>
2021-03-02 08:25:49 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ $(function () {
   // ドロワー
   $('.header__button').on('click', function () {
     $('html, body').css('position', 'fixed');
-    $('.header').addClass("close");
+    $('.header').addClass("dn");
     $('.drawer').fadeIn();
     return false;
   });
   $('.drawer__button').on('click', function () {
     $('.drawer').fadeOut();
     $('html, body').css('position', 'static');
-    $('.header').removeClass("close");
+    $('.header').removeClass("dn");
     return false;
   });
 
@@ -46,32 +46,7 @@ $(function () {
   // }
   // new Swiper('.swiper-container', swipeOption);
 
-  // モーダル
-  // $('.header__button, .drawer__button').on('click', function () {
-  //   $('.modal').fadeIn();
-  //   return false;
-  // });
-  // $('.modal__icon, .modal__wrapper').on('click', function () {
-  //   $('.modal').fadeOut();
-  //   return false;
-  // });
 
-  //★タブクリックでコンテンツ差し替え//
-  // ①タブをクリックしたら発動
-  $('.info__tab').click(function () {
-
-    // ②クリックされたタブの順番を変数に格納
-    var index = $('.info__tab').index(this);
-
-    // ③クリック済みタブのデザインを設定したcssのクラスを一旦削除
-    $('.info__tab').removeClass('active');
-
-    // ④クリックされたタブにクリック済みデザインを適用する
-    $(this).addClass('active');
-
-    // ⑤コンテンツを一旦非表示にし、クリックされた順番のコンテンツのみを表示
-    $('.info__container').removeClass('show').eq(index).addClass('show');
-  });
 
 
 });
